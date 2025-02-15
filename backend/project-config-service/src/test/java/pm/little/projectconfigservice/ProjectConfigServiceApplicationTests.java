6486cf6631ec640ebb865f97c78ee73d928ca28a
Damir @dmr4eg <fifth.dmr@gmail.com>
2025-02-16 00:50:57 +0100

    :memo: - Updated a structure

---
+++ backend/project-config-service/src/test/java/pm/little/projectconfigservice/ProjectConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pm.little.projectconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectConfigServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
