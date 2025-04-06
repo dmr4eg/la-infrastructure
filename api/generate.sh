6a28b0ae0fdee66a37dcf29f2e36479e9b3db1b6
Damir @dmr4eg <fifth.dmr@gmail.com>
2025-04-06 03:11:50 +0200

    changed component id name

--- api/generate.sh
+++ api/generate.sh
@@ -1,1 +1,5 @@
-openapi-generator generate -i ./public.openapi.yaml -g spring -o gen/ --additional-properties=apiPackage=pm.little.api.controllers,basePackage=pm.little.api,configPackage=pm.little.api.confs,modelPackage=pm.little.api.models
+#openapi-generator generate -i ./public.openapi.yaml -g spring -o gen/ --additional-properties=apiPackage=pm.little.api.controllers,basePackage=pm.little.api,configPackage=pm.little.api.confs,modelPackage=pm.little.api.models
+
+#client
+openapi-generator generate -i ./public.openapi.yaml -g java -o gen/ --additional-properties=modelPackage=pm.little.client.project.models,invokerPackage=pm.little.client.project.invokers,apiPackage=pm.little.client.project.defaultapi
+
