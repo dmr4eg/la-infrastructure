c8889fce12416ae087dd8a90aeeb410870924262
Damir @dmr4eg <fifth.dmr@gmail.com>
2025-04-21 18:59:11 +0200

    updated generate sh

--- api/generate.sh
+++ api/generate.sh
@@ -1,5 +1,8 @@
-#openapi-generator generate -i ./public.openapi.yaml -g spring -o gen/ --additional-properties=apiPackage=pm.little.api.controllers,basePackage=pm.little.api,configPackage=pm.little.api.confs,modelPackage=pm.little.api.models
+#spring
+#openapi-generator generate -i ./public.openapi.yaml -g spring -o gen/spring/ --additional-properties=apiPackage=pm.little.api.controllers,basePackage=pm.little.api,configPackage=pm.little.api.confs,modelPackage=pm.little.api.models
 
-#client
-openapi-generator generate -i ./public.openapi.yaml -g java -o gen/ --additional-properties=modelPackage=pm.little.client.project.models,invokerPackage=pm.little.client.project.invokers,apiPackage=pm.little.client.project.defaultapi
+#java-client
+#openapi-generator generate -i ./public.openapi.yaml -g java -o gen/ --additional-properties=modelPackage=pm.little.client.project.models,invokerPackage=pm.little.client.project.invokers,apiPackage=pm.little.client.project.defaultapi
 
+#fe-js
+openapi-generator generate -i ./public.openapi.yaml -g javascript -o gen/js/ --additional-properties=apiPackage=controllers,invokerPackage=openapi,modelPackage=models
