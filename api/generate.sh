5035be3ea3abe1550cecb68dba9d681412960e57
Damir @dmr4eg <fifth.dmr@gmail.com>
2025-04-24 02:53:05 +0200

    updated generate sh for fe

--- api/generate.sh
+++ api/generate.sh
@@ -1,8 +1,11 @@
 #spring-content
-openapi-generator generate -i ./public.content.openapi.yaml -g spring -o gen/spring/content --additional-properties=apiPackage=pm.little.api.controllers,basePackage=pm.little.api,configPackage=pm.little.api.confs,modelPackage=pm.little.api.models
+#openapi-generator generate -i ./public.content.openapi.yaml -g spring -o gen/spring/content --additional-properties=apiPackage=pm.little.api.controllers,basePackage=pm.little.api,configPackage=pm.little.api.confs,modelPackage=pm.little.api.models
 
 #spring-course
-openapi-generator generate -i ./public.course.openapi.yaml -g spring -o gen/spring/course --additional-properties=apiPackage=pm.little.api.controllers,basePackage=pm.little.api,configPackage=pm.little.api.confs,modelPackage=pm.little.api.models
+#openapi-generator generate -i ./public.course.openapi.yaml -g spring -o gen/spring/course --additional-properties=apiPackage=pm.little.api.controllers,basePackage=pm.little.api,configPackage=pm.little.api.confs,modelPackage=pm.little.api.models
 
-#fe-js
-#openapi-generator generate -i ./public.openapi.yaml -g javascript -o gen/js/ --additional-properties=apiPackage=controllers,invokerPackage=openapi,modelPackage=models
+#fe-js-content
+openapi-generator generate -i ./public.content.openapi.yaml -g javascript -o gen/js/content --additional-properties=apiPackage=controllers,invokerPackage=api-content-service,modelPackage=models
+
+#fe-js-course
+openapi-generator generate -i ./public.course.openapi.yaml -g javascript -o gen/js/course --additional-properties=apiPackage=controllers,invokerPackage=api-course-service,modelPackage=models
