f9eac9e617b8f0599d73e5d7ccac59e72d2a6cf7
Damir @dmr4eg <fifth.dmr@gmail.com>
2025-04-27 01:21:25 +0200

    updated generators

--- api/generate.sh
+++ api/generate.sh
@@ -5,7 +5,15 @@
 #openapi-generator generate -i ./public.course.openapi.yaml -g spring -o gen/spring/course --additional-properties=apiPackage=pm.little.api.controllers,basePackage=pm.little.api,configPackage=pm.little.api.confs,modelPackage=pm.little.api.models
 
 #fe-js-content
-openapi-generator generate -i ./public.content.openapi.yaml -g javascript -o gen/js/content --additional-properties=apiPackage=controllers,invokerPackage=api-content-service,modelPackage=models
+openapi-generator generate \
+    -i ./content.openapi.yaml \
+    -g typescript-fetch \
+    -o gen/ts/content \
+    --additional-properties=apiPackage=controllers,invokerPackage=api-content-service,modelPackage=models
 
 #fe-js-course
-openapi-generator generate -i ./public.course.openapi.yaml -g javascript -o gen/js/course --additional-properties=apiPackage=controllers,invokerPackage=api-course-service,modelPackage=models
+openapi-generator generate \
+    -i ./course.openapi.yaml \
+    -g typescript-fetch \
+    -o gen/ts/course \
+    --additional-properties=apiPackage=controllers,invokerPackage=api-course-service,modelPackage=models
