74b58ac3d55810b0651e01677c242d6567326d49
Damir @dmr4eg <fifth.dmr@gmail.com>
2025-06-07 13:48:21 +0200

    dns

--- terraform/modules/shared/0_data.tf
+++ terraform/modules/shared/0_data.tf
@@ -3,6 +3,6 @@ data "azurerm_client_config" "this" {}
 data "azurerm_resource_group" "this" { name = "rg-little-pm" }
 
 data "azurerm_dns_zone" "this" {
-  name                = "app.little.pm"
+  name                = "app.dmr4eg.xyz"
   resource_group_name = data.azurerm_resource_group.this.name
 }
